be3df3f800103aabaddc525b31df55729ffd98d0
zubanov.d <zubanov@gmail.com>
2021-10-23 10:41:35 +0300

    Init repo

---
+++ launcher/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	java := path + "\\lib\\jre\\bin\\java.exe"
+	fmt.Println(java)
+
+	jar := path + "\\lib\\converter.jar"
+	fmt.Println(jar)
+
+	cmd := exec.Command(java, "-jar", jar)
+	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 0x08000000}
+
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(stdout))
+}
